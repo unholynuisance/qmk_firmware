019283a1469e490857a6b29af8c80445cb11f4fd
unholynuisance <mtataryn555@gmail.com>
2024-11-01 18:15:50 +0200

    add custom myrioku-colemak-dh layout for moonlander

---
+++ keyboards/zsa/moonlander/keymaps/miryoku-colemak-dh/config.h
@@ -0,0 +1,22 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 175
+
+#define PERMISSIVE_HOLD
+
+#define QUICK_TAP_TERM 0
+
+#undef RGB_MATRIX_TIMEOUT
+#define RGB_MATRIX_TIMEOUT 900000
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define CAPS_LOCK_STATUS
+#define SERIAL_NUMBER "5wJYn/DMn6P"
+#define LAYER_STATE_16BIT
+
+#define RGB_MATRIX_STARTUP_SPD 60
+
